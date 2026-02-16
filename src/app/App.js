9be4ce32beccbd465c285ba81eba4e2856070fc9
Emmanuel B <blancocemmanuel@gmail.com>
2026-02-16 21:26:37 +1100

    router

--- src/app/App.js
+++ src/app/App.js
@@ -19,10 +19,15 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path='about' element= {<About/>} />
     <Route path='sign-up' element={<SignUp/>} />
     <Route path='articles' element={<Articles/>} />
-    <Route path='categories' element={<Categories/>} />
+
+    <Route path='categories' element={<Categories/>}>
+    <Route path=':name' element={<Category/>}/>{/*parent-child + nested*/}
+    </Route>
+  
     <Route path='profile' element={<Profile/>} >
     <Route path='edit' element={<EditProfileForm/>}/>
     </Route>{ /*for this profile parent-child React Router combines them to create /profile/edit in the URL. You do not need to add the / in the child path—React Router handles it for you. */ }
+
     <Route path='articles/:title' element={<Article/>} />
     <Route path='authors/:name' element={<Author/>} />
    
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 import { Outlet } from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 export default function Profile () {
   const user = useSelector(selectCurrentUser)
